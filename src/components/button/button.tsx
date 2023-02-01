6edcd9007a173d828b0a9e8ba3975ae2abe6dc09
Ananasovna <a.n.ushkova@yandex.ru>
2023-02-01 12:55:18 +0200

    refactor: extract button into separate Component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,11 +1,14 @@
 import cc from 'classcat';
+
 interface ButtonProps {
-    title:string;
-    className?:string;
+  title: string;
+  className?: string;
 }
 
-export const Button = ({title,className}:ButtonProps)=><button className={cc(
-  ['h-14 w-60 rounded border border-emerald-900 text-2xl font-medium transition-all hover:bg-violet-700 hover:text-white',className])}>
-  {title}
-</button>;
+export const Button = ({ title, className }: ButtonProps) =>
+  <button className={cc(
+    ['rounded-full hover:shadow-xl font-medium', className]
+  )}>
+    {title}
+  </button>;
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
+import { Button } from "src/components/button/button";
 import { BUTTON_ARR } from "src/constants/main";
+
 
 export default function Page() {
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center m-0 pt-12 bg-brown_light 
-    bg-[url('../../public/image/img/bgMain.png')]" >
+    <div className="w-screen h-screen flex justify-center items-center m-0 pt-12 bg-mainPageBg" >
       <div className="flex justify-between gap-10 text-white  m576:text-xl">
-        {BUTTON_ARR.map(button => <button key={button.id} 
-        className="rounded-full py-4 px-7 bg-red hover:bg-green hover:shadow-xl font-medium">
-          {button.title}
-        </button>)}
-      </div> 
+        {BUTTON_ARR.map(el => <Button key={el.id} title={el.title}
+          className="py-4 px-7 bg-red hover:bg-green font-medium" />)}
+      </div>
     </div>
   );
 }
