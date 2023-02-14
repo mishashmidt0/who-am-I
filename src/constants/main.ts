e62dadd8d22307ca28901a1ecf434444b17a9765
Ananasovna <a.n.ushkova@yandex.ru>
2023-02-14 10:56:01 +0200

    feat: implement decksSettings open function

--- src/constants/main.ts
+++ src/constants/main.ts
@@ -1,5 +1,5 @@
 export const BUTTON_ARR = [
   {id:0, path:'', title: 'Начать'},
-  {id:1, path:'', title: 'Создать колоду'},
+  {id:1, path:'', title: 'Редактировать колоды'},
   {id:2, path:'', title: 'Настройки'},
 ];
